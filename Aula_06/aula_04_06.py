82975b4a2a03a1ba091a5cb8384d85db99fe7245
Matheusleonel1 <matheussmv72@gmail.com>
2025-04-06 18:27:59 -0300

    Update aula_04_06.py

--- Aula_06/aula_04_06.py
+++ Aula_06/aula_04_06.py
@@ -1,10 +1,26 @@
 # 4- Crie um programa que:
 # 1- Peça ao usuário para digitar dez números inteiros e os armazene em uma lista.
 # 2- Exiba a lista completa.
-# 3- Exiba o maior e o menor número da lista.
+# 3- Exiba o maior e o menor número da lista
 # 4- Exiba a soma e a média de todos os números.
 
-# Entrada de dados
+# Entrada dos dados
+numeros = []
+for i in range(10):
+    num = int(input("Digite um número inteiro: "))
+    numeros.append(num)
 
-n = 1
-num = []
+# Processamento de dados 
+maior = max(numeros)
+menor = min(numeros)
+soma = sum(numeros)
+media = soma / len(numeros)
+
+
+#Saida de dados
+print("\nLista completa dos números:")
+print(numeros)
+print(f"\nMaior número: {maior}")
+print(f"Menor número: {menor}")
+print(f"\nSoma dos números: {soma}")
+print(f"Média dos números: {media:.2f}")
